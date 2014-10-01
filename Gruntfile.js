fdaae0a8e78c799f9dfc7f1890374e07d03177f5
Ben Styles <ben@arcpublications.co.uk>
2014-10-01 15:07:00 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    "parse-phrases": {
+      options: {
+        phrase_delimiter: '|',
+        option_delimiter: '`'
+      },
+      target1: {
+        src: 'abc.txt'
+      }
+    }
+  });
+  grunt.registerMultiTask('parse-phrases', function() {
+    var options=this.options({
+          phrase_delimiter: '|',
+          option_delimiter: '`'
+        }),
+        phraseDelimiter=options.phrase_delimiter,
+        optionDelimiter=options.option_delimiter,
+        srcFilePath=this.files[0].src,
+        fileString=grunt.file.read(srcFilePath).trim(),
+        cleanFileString=fileString.lastIndexOf(phraseDelimiter,0)===0 ?
+          fileString.slice(1) : fileString,
+        phraseArray=cleanFileString.split(phraseDelimiter);
+    grunt.log.writeln(phraseArray);
+    grunt.log.writeln('Array has '+phraseArray.length+' elements.');
+
+  });
+  grunt.registerTask('default', ['parse-phrases']);
+};
