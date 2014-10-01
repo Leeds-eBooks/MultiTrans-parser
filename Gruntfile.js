db45d9581c7614b0a7d10992706f77edd6b19e4f
Ben Styles <ben@arcpublications.co.uk>
2014-10-01 15:13:28 +0100

    Added todos

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,12 +19,12 @@ module.exports = function(grunt) {
         optionDelimiter=options.option_delimiter,
         srcFilePath=this.files[0].src,
         fileString=grunt.file.read(srcFilePath).trim(),
-        cleanFileString=fileString.lastIndexOf(phraseDelimiter,0)===0 ?
+        cleanFileString=fileString.lastIndexOf(phraseDelimiter,0)===0 ? // TODO: needs to handle newlines too
           fileString.slice(1) : fileString,
         phraseArray=cleanFileString.split(phraseDelimiter);
     grunt.log.writeln(phraseArray);
     grunt.log.writeln('Array has '+phraseArray.length+' elements.');
-
+    // TODO
   });
   grunt.registerTask('default', ['parse-phrases']);
 };
