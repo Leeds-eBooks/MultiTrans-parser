8600e41c4a5bcb38c0ea457ae4e7f18117f60557
Ben Styles <enquiries@leeds-ebooks.co.uk>
2016-03-02 15:12:09 +0000

    Improvements to UX

--- single.js
+++ single.js
@@ -1,3 +1,24 @@
 import parser from './parser'
+import chalk from 'chalk'
+import CSON from 'cson'
+import clip from 'to-clipboard'
 
-console.log(JSON.stringify(parser(process.argv[2]).content))
+const {content, title, path} = parser(process.argv[2]),
+      cson = CSON.stringify(content, null, 2);
+
+console.log(chalk.green('Path:'), path)
+console.log(chalk.green('Title:'), title)
+console.log('')
+console.log(chalk.green('Content:'))
+console.log(cson)
+
+try {
+  clip.sync(cson)
+  console.log('')
+  console.log(chalk.green('✔︎ Copied to clipboard'))
+  console.log('')
+} catch (e) {
+  console.log('')
+  console.error(chalk.red('Copying to clipboard FAILED', e))
+  console.log('')
+}
